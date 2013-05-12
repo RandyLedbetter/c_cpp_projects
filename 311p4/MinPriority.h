8d070a5f2992452ea414bc746903e571a58a371b
RandyLedbetter <rledbetter@mail.csuchico.edu>
2013-05-12 14:23:27 -0700

    Initial commit of 311p4 project

---
+++ 311p4/MinPriority.h
@@ -0,0 +1,28 @@
+// File Name: MinPriority.h
+// Author: Randy Ledbetter
+// Class: CSCI 311
+// Instructor: Dr. Judith Challinger
+// Date: 05/11/2013
+
+/*
+===============================================================================
+Description: This is the header file MinPriority.h. It represents the interface
+             file for a minimum priority queue.
+===============================================================================
+*/
+
+#ifndef H_MIN_PRIORITY
+#define H_MIN_PRIORITY
+
+class MinPriority
+{
+public:
+
+
+
+private:
+
+
+};
+
+#endif
